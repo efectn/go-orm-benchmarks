b119f6bc37fa0a1782dfd2a8bb3792371e474404
Muhammed Efe Çetin <efectn@protonmail.com>
2023-06-23 17:50:38 +0300

    fix results generation

--- .github/run_benchmarks_actions.sh
+++ .github/run_benchmarks_actions.sh
@@ -8,10 +8,13 @@ go build main.go
 
 # Apply output to template
 logs=$(./main -source="host=localhost user=postgres password=postgres dbname=test sslmode=disable" -debug=false)
-escaped=$(echo "${logs}" | sed '$!s@$@\\@g')
-
-proto=$(sed "s|@(benchmark-results)|${escaped}|g" <<< $proto)
 
 # Print final results & delete benchmarker
 rm main
-echo $proto
+
+echo """# Results
+
+- orm-benchmark (with no flags)
+\`\`\`
+${logs}
+\`\`\`""" > results.md
